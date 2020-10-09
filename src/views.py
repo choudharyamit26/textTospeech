6c977ad7a888f43b5746583204a039b1e9d73f7a
Amit choudhary <ravichoudhary766@gmail.com>
2020-10-09 17:07:41 +0530

    Implemented OCR for pdf reading

--- src/views.py
+++ src/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View, CreateView, ListView
-
+from django.core.files.storage import FileSystemStorage
 from .forms import Importdataform
 from .models import Audio
 from django.http import HttpResponseRedirect, HttpResponse
@@ -20,6 +20,11 @@ from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfparser import PDFParser
 
+from pdf2image import convert_from_path
+import pytesseract
+import argparse
+import cv2
+from PIL import Image
 
 # Create your views here.
 
@@ -34,32 +39,46 @@ class CreateAudio(View):
 
     def post(self, request, *args, **kwargs):
         in_file = self.request.FILES['document']
-        print(in_file)
-        arabic_txt = ''
-        output_string = StringIO()
+        fs = FileSystemStorage()
+        filename = fs.save(in_file.name, in_file)
+        text = ''
+        page = convert_from_path(filename, 400)
+        for i, image in enumerate(page):
+            print(image)
+            fname = "pdf" + str(i) + ".png"
+            image.save(fname, "PNG")
+            img = cv2.imread(fname)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+            cv2.medianBlur(img, 3)
+            text = pytesseract.image_to_string(img, lang='ara')
+            print(text)
+        # print(in_file)
+        # arabic_txt = ''
+        # output_string = StringIO()
         # with open(uploaded_file, 'rb') as in_file:
-        a = []
-        parser = PDFParser(in_file)
-        doc = PDFDocument(parser)
+        # a = []
+        # parser = PDFParser(in_file)
+        # doc = PDFDocument(parser)
         # codec = 'utf-8'
-        rsrcmgr = PDFResourceManager()
-        device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        for page in PDFPage.create_pages(doc):
-            interpreter.process_page(page)
-        text = output_string.getvalue()
-        a = a.append(text)
-        arabic_txt = ''.join(text)
-        print(arabic_txt)
-        in_file.close()
-        device.close()
-        output_string.close()
-        text = text.encode('utf-8')
-        output = open('output.txt', 'wb')
+        # rsrcmgr = PDFResourceManager()
+        # device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+        # interpreter = PDFPageInterpreter(rsrcmgr, device)
+        # for page in PDFPage.create_pages(doc):
+        #     interpreter.process_page(page)
+        # text = output_string.getvalue()
+        # a = a.append(text)
+        # arabic_txt = ''.join(text)
+        # print(arabic_txt)
+        # in_file.close()
+        # device.close()
+        # output_string.close()
+        # text = text.encode('utf-8')
+        # output = open('output.txt', 'wb')
         # print(text.decode('utf-8'))
         # text = text.decode('utf-8')
-        output.write(text)
-        output.close()
+        # output.write(text)
+        # output.close()
         # return text
         # print(text)
 
@@ -69,7 +88,7 @@ class CreateAudio(View):
         polly = session.client("polly")
         try:
             # Request speech synthesis
-            response = polly.synthesize_speech(Text=arabic_txt,
+            response = polly.synthesize_speech(Text=text,
                                                OutputFormat="mp3",
                                                VoiceId="Zeina")
             x = self.model.objects.all().last()
